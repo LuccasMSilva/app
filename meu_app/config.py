d585f7d9e80569e4670d360d59b854d7ef8d341b
LuccasMSilva <Luccas-ms@hotmail.com>
2025-10-31 16:39:08 -0300

    Delete meu_app directory

--- meu_app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-super-secreta')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
